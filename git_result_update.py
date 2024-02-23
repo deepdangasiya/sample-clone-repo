fd543a60095a12652b1500dcc9df7eae5f111649
deepdangasiya <deep.dangasiya@softnautics.com>
2024-02-23 17:36:53 +0530

    debug commit to test

--- git_result_update.py
+++ git_result_update.py
@@ -7,8 +7,8 @@ import argparse
 
 def git_readme_update(readme_data, branch):
     repo = git.Repo(os.getcwd())
-    repo.git.checkout(branch)
-    print(f"active branch:: {repo.active_branch}")
+    # repo.git.checkout(branch)
+    # print(f"active branch:: {repo.active_branch}")
     content = ("### Test Results: ###\n\n"
                "| Test-ID | Test Case Name | Test Case Result |\n"
                "| :------:|:--------------:|:----------------:|\n")
